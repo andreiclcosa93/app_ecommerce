6f2a3fa9b5072dee4703d5eec5c8b07d9f562adb
andreiclcosa93 <andrei.cosa93@gmail.com>
2023-10-28 21:17:14 +0300

    add header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+import image1 from '../assets/images/logo.png';
+import image2 from '../assets/images/shopping-cart.png';
+// import {ReactComponent as ShopCart} from '../assets/images/shopping-cart.svg';
+
+import './Header.css';
+
 function Header() {
     return (
-        <div>
-            < Link to="/">Home</Link>
-            < Link to="/login">Login</Link>
+        <div className="header bg-dark shadow-lg d-flex justify-content-between align-items-center px-5 py-3 mb-5">
+            <div>
+                < Link to="/">
+                    <img src={image1} alt="" className="logo" /> 
+                </Link>
+                    <span className="logoName">AAn.</span>
+            </div>
+            <div>
+                < Link to="/login" className="login">Login</Link>
+                {/* <ShopCart /> */}
+                <img src={image2} alt="" className="shopCart" />
+            </div>
         </div>
     );
 }
